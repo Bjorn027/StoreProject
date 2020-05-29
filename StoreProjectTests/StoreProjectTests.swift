6132646e19df2037cc2826e4ed0ff8b144c7ff52
Shawn <shawn.boreland@easternstudent.ca>
2020-05-29 10:07:19 -0300

    first commit

---
+++ StoreProjectTests/StoreProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  StoreProjectTests.swift
+//  StoreProjectTests
+//
+//  Created by Shawn on 2020-05-28.
+//  Copyright © 2020 Shawn. All rights reserved.
+//
+
+import XCTest
+@testable import StoreProject
+
+class StoreProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
